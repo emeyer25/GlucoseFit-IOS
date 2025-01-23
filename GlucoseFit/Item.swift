e537cb20ce7cd299a5a89a418a9a98486bd3c8c0
eric <emeyer2025@gmail.com>
2025-01-23 08:44:01 -0500

    Initial Commit

---
+++ GlucoseFit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GlucoseFit
+//
+//  Created by Eric Meyer on 1/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

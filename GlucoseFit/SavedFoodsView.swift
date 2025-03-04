4462b24358f55c793ace24690c87336fcefd2150
eric <emeyer2025@gmail.com>
2025-03-04 09:29:56 -0500

    SavedFoodList finally works. - in mealLogEntry I created SavedFoodItem instead of making it take the Fooditem.

---
+++ GlucoseFit/SavedFoodsView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedFoodsView.swift
+//  GlucoseFit
+//
+//  Created by Eric Meyer on 2/20/25.
+//
+
+import SwiftUI
+
+struct SavedFoodsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedFoodsView()
+}
